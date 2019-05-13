5b226f4321506c314def61c21758cf8c5d11bfa5
VollkornWurst <simon.blaha@gmx.at>
2019-05-14 01:36:43 +0200

    Krypto added to server

--- Programs/Server/rest-server/login/model.js
+++ Programs/Server/rest-server/login/model.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const User = require('../user/model');
 const randtoken = require('rand-token');
 const { userDatabase } = require('../config');
+const bcryp = require('bcrypt');
+const saltRounds = 10;
 
 mongoose.connect(userDatabase, { useNewUrlParser: true });
 
@@ -10,6 +12,19 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("Connected to database!");
 });
+
+/*
+* Erstellt einen User mit dem Passwort Wurst und dem Usernamen Hans
+bcryp.hash("Wurst", saltRounds, function(err, hash) {
+    if(err) {
+        console.log(err);
+    } else {
+        let newUser = new User({ name: "Hans", password: hash });
+        newUser.save();
+        console.log("ID des erstellten Users: " + newUser._id);
+    }
+});
+*/
 
 function generateToken() {
     let uid = randtoken.uid;
@@ -23,12 +38,14 @@ function getToken(id, password) {
         User.findById(id, function(err, user) {
             if(err) {
                 reject({error: "Invalid userid or password"});
-            } else
-            if(user.password == password && user._id == id) {
-                user.token = token;
-                user.save(); 
-                resolve({ token: user.token });
-            } else reject({error: "Invalid userid or password"});
+            } else {
+                const res = bcryp.compareSync(password, user.password);
+                if(res && user._id == id) {
+                    user.token = token;
+                    user.save(); 
+                    resolve({ token: user.token });
+                } else reject({error: "Invalid userid or password"});
+            }
         });
     });
 }
