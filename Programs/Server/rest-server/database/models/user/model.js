012f36de22dd0cd10ed0090a9deb3d96711e2523
VollkornWurst <simon.blaha@gmx.at>
2019-06-20 21:34:03 +0200

    update

--- Programs/Server/rest-server/database/models/user/model.js
+++ Programs/Server/rest-server/database/models/user/model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     token: {
         type: String,
-        required: true
+        required: false
     }
 });
 
--- Programs/Server/rest-server/user/register/controller.js
+++ Programs/Server/rest-server/user/register/controller.js
@@ -10,8 +10,8 @@ function registerAction(request, response) {
     }
 
     model.registerUser(jsonRequest.username, jsonRequest.password).then(function(result) {
+        console.log("Benutzer anglegt: " + JSON.stringify(result));
         response.json(result);
-        console.log("Benutzer anglegt: " + result);
     }, function(error) {
         console.log("Benutzer konnte nicht angelegt werden: " + JSON.stringify(request.body));
         response.status(500).json(error);
